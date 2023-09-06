64717b0b18acbfd20b8e4ed2e0c69106791488bf
Peter <85614837+underSkord@users.noreply.github.com>
2023-09-06 17:42:35 +0200

    delete json files

--- app.js
+++ app.js
@@ -1,18 +1,2 @@
 const nav = document.querySelector("nav");
 const langBtn = document.querySelector("#language__selector");
-
-langBtn.addEventListener("click", () => {
-  changeLanguage();
-});
-
-function changeLanguage() {
-  console.log(":)))))");
-}
-
-document.addEventListener("scroll", () => {
-  if (document.documentElement.scrollTop > 0) {
-    nav.classList.add("menu--alt");
-  } else {
-    nav.classList.remove("menu--alt");
-  }
-});
