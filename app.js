df24f7491013d61e053e6e1f5de9be9e3cf6d19d
Peter <85614837+underSkord@users.noreply.github.com>
2023-09-12 11:28:35 +0200

    add breakpoints/ mixins

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+"use strict";
+
 const nav = document.querySelector("nav");
+const navItems = document.querySelector("#nav__items");
 const langBtn = document.querySelector("#language__selector");
+
+// solve how to translate the site using JS. tried json but having nested elements AND applying ids and classes to them is out of my realm
