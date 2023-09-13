5418ef86a11dd9168b32176acf24bb95f6a8f558
Peter <85614837+underSkord@users.noreply.github.com>
2023-09-13 10:36:21 +0200

    add text and styling to skillset header

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Personal Website (will go here, once it's done)
 https://underskord.github.io/
+
+-- TODO --
+
+- come up with a solution to the language selector image. currently it uses an npm package, but github can't use those.
+
+- add functionality to buttons etc.
+
+- in the footer make it so you cant select the whole element for email, linkedin and github (currently these link elements have a width of 100%, so make them narrower)
--- app.js
+++ app.js
@@ -5,3 +5,26 @@ const navItems = document.querySelector("#nav__items");
 const langBtn = document.querySelector("#language__selector");
 
 // solve how to translate the site using JS. tried json but having nested elements AND applying ids and classes to them is out of my realm
+let currentLanguage = "en";
+console.log(currentLanguage);
+
+langBtn.addEventListener("click", () => {
+  currentLanguage = currentLanguage === "en" ? "hu" : "en";
+  languageCheck(currentLanguage);
+  console.log(currentLanguage);
+});
+
+function languageCheck(currentLanguage) {
+  switch (currentLanguage) {
+    case "en":
+      console.log("its a da enrishu");
+      break;
+
+    case "hu":
+      console.log("its a da hurishu");
+      break;
+  }
+}
+
+const h2 = document.querySelector("h2");
+const p = document.querySelectorAll("p");
